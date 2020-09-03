1984b81eb536c28f315c2a791214a5c928b98509
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-09-03 12:27:17 +0200

    Ajout de l'activité detail

--- app/src/main/java/com/example/applicationcdev/DescriptionPlatActivity.java
+++ app/src/main/java/com/example/applicationcdev/DescriptionPlatActivity.java
@@ -3,7 +3,11 @@ package com.example.applicationcdev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class DescriptionPlatActivity extends AppCompatActivity {
 
@@ -12,6 +16,21 @@ public class DescriptionPlatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description_plat);
         Plat p = (Plat) this.getIntent().getSerializableExtra("plat");
-        Toast.makeText(this,p.getNom(),Toast.LENGTH_SHORT).show();
+
+        TextView  tvTitre = findViewById(R.id.detail_plat_titre);
+        TextView  tvDescr = findViewById(R.id.detail_plat_description);
+        ImageView imImage = findViewById(R.id.detail_plat_image);
+
+        tvTitre.setText(p.getNom());
+        tvDescr.setText(p.getDescription());
+        imImage.setImageResource(getDrawableRessourceID(p.getImage()));
+
+
+    }
+
+    private int getDrawableRessourceID(String name) {
+        //nameResourceID = R.drawable.[name]
+        int nameResourceID = this.getResources().getIdentifier(name, "drawable", this.getApplicationInfo().packageName);
+        return nameResourceID;
     }
 }
